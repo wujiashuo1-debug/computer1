ffa4c39f2df8915627e802e08834b218f77b27fd
HSapple <wujiashuo1@gmail.com>
2026-07-05 22:09:38 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Lightweight service worker: keep V4 file structure, but avoid aggressive caching.
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  return;
+});
